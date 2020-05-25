b1718314c230b562f178e167ad1a42421ce3b726
Lalitus <tallitosan@gmail.com>
2020-05-24 19:03:04 -0500

    restServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,21 @@
 // ==========
 // puerto
 // ==========
-
 process.env.PORT = process.env.PORT || 3000;
+
+// ==========
+// entorno
+// ==========
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==========
+// db
+// ==========
+let urlDb;
+if(process.env.NODE_ENV === 'dev'){
+    urlDb = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDb = 'mongodb+srv://lalo:CGrYyyRoYqorm5tR@cluster0-qhfig.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDb;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 require('./config/config');
+
 const express = require('express');
+//  Using Node.js `require()`
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -7,39 +10,34 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+app.use(require('./routes/usuario'));
  
-app.get('/usuario', function (req, res) {
-    res.json('get usuario');
-});
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
+mongoose.connect(
+    process.env.URLDB,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex:true
+    },
+    (err, res) => {
+        if(err){
+            throw err;
+        }
 
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        });
-    }else{
-        res.json({
-            persona: body
-        });
-
-    }
-});
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario');
+    console.log('Base de datos online');
 });
  
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
+
+
+// usr:
+// lalo
+// pass:
+// CGrYyyRoYqorm5tR
+
+// atlas
+// mongodb+srv://lalo:CGrYyyRoYqorm5tR@cluster0-qhfig.mongodb.net/cafe
