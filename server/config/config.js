7488353e38d4fc574de8f9b28874a11be67a6725
Lalitus <tallitosan@gmail.com>
2020-05-24 19:32:34 -0500

    fixed env variable MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDb;
 if(process.env.NODE_ENV === 'dev'){
     urlDb = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDb = 'mongodb+srv://lalo:CGrYyyRoYqorm5tR@cluster0-qhfig.mongodb.net/cafe';
+    urlDb = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDb;
